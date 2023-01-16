e8ca1aebc881ca6113fc960c45c94b57a3459884
Hassan Khan <hassankpisces@gmail.com>
2023-01-17 01:37:30 +0500

    hassan

--- OnlineShoppingStore/Controllers/PaymentController.cs
+++ OnlineShoppingStore/Controllers/PaymentController.cs
@@ -14,6 +14,7 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 using PayPal.Api;
+using Antlr.Runtime;
 
 namespace OnlineShoppingStore.Controllers
 {
@@ -22,7 +23,7 @@ namespace OnlineShoppingStore.Controllers
         // GET: Payment
         public ActionResult PaymentWithPapal2()
         {
-            return View();
+            return PartialView();
         }
         [HttpPost]
         public ActionResult PaymentWithPapal2(Shippingdetail shippingdetail)
@@ -30,6 +31,7 @@ namespace OnlineShoppingStore.Controllers
             dbMyOnlineShoppingEntities db = new dbMyOnlineShoppingEntities();
 
             Tbl_ShippingDetails tbl = new Tbl_ShippingDetails();
+
             tbl.Adress = shippingdetail.Adress;
             tbl.MemberId = shippingdetail.MemberId;
             tbl.City = shippingdetail.City;
@@ -41,57 +43,6 @@ namespace OnlineShoppingStore.Controllers
 
             db.Tbl_ShippingDetails.Add(tbl);
             db.SaveChanges();
-
-            /* Item cart = (Item)Session["cart"];
-             var shippingdetail = new Shippingdetail()
-             {
-                 ShippingDetailId = 79,
-                 MemberId = 89,
-                 Adress = frc["Adress"],
-                 City = frc["City"],
-                 State = frc["State"],
-                 Country = frc["Country"],
-                 ZipCode = frc["ZipCode"],
-                 OrderId = 10000,
-                 AmountPaid = 10000,
-                 PaymentType = "Cash",
-             };
-             ctx.Tbl_ShippingDetails.Add(shippingdetail);
-             ctx.SaveChanges();*/
-
-
-            /*var Adress = Request.Form["Adress"];
-            var City = Request.Form["City"];
-            var State = Request.Form["State"];
-            var Country = Request.Form["Country"];
-            var Zipcode = Request.Form["Zipcode"];
-
-
-            var db = ctx.Open("dbMyOnlineShoppingEntities");
-            var insertCommand = "INSERT INTO Tbl_ShippingDetails (Adress, City, State, Country, Zipcode) Values(@0, @1, @2, @3, @4)";
-            db.Execute(insertCommand, Adress, City, State, Country, Zipcode);
-            Response.Redirect("~/SuccessView");*/
-
-
-            /*SqlConnection conn = new SqlConnection("Data Source=DESKTOP-M407LSF;Initial Catalog=dbMyOnlineShopping;Integrated Security=True");
-
-                conn.Open();
-
-                SqlCommand cmd = new SqlCommand("spAddOrder", conn);
-                cmd.CommandType = CommandType.StoredProcedure;*/
-
-
-
-            /*cmd.Parameters.AddWithValue("@Adress", shippingdetail.Adress);
-            cmd.Parameters.AddWithValue("@MemberId", shippingdetail.MemberId);
-
-
-                        SqlParameter paramAdress = new SqlParameter();
-                        paramAdress.ParameterName = "@Adress";
-                        paramAdress.Value = shippingdetail.Adress;
-                        cmd.Parameters.Add(paramAdress
-            cmd.ExecuteNonQuery();
-            conn.Close();*/
 
             return RedirectToAction("Index","Home");
         }
